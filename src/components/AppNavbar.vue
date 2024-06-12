6256802f419387a2d821d7528f743a54a224a394
wubo <2551921037@qq.com>
2024-06-13 00:05:44 +0800

    初始导航栏加图片

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -1,6 +1,8 @@
 <template>
+
   <nav class="navbar">
     <div class="navbar-container">
+      <img src="@/assets/九寨沟logo.png" alt="九寨沟Logo" class="navbar-logo">
       <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal">
         <el-menu-item index="/" @click="navigate('/')">首页</el-menu-item>
         <el-menu-item index="/about" @click="navigate('/about')">关于九寨</el-menu-item>
@@ -12,6 +14,7 @@
       </el-menu>
     </div>
   </nav>
+
 </template>
 
 <script>
@@ -35,16 +38,45 @@ export default {
 </script>
 
 <style scoped>
-.el-menu-demo {
-  background-color: #545c64;
-  color: white;
-}
 .navbar {
-  position: fixed; /* 固定导航栏 */
+  position: fixed;
   top: 0;
   left: 0;
   width: 100%;
-  z-index: 999; /* 确保导航栏在最上层 */
-  background-color: #545c64; /* 背景色，可根据需要调整 */
+  z-index: 999;
+  background-color: #fff; /* 你可以选择一个你喜欢的颜色 */
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); /* 添加一些阴影，使导航栏看起来更加立体 */
+}
+.navbar-logo {
+  height: 50px; /* 根据需要调整 Logo 图片的高度 */
+  margin-right: 10px; /* 根据需要添加右边距，使 Logo 与菜单项之间有一定间距 */
+  margin-left: 10px; /* 新增左边距，调整 Logo 位置 */
+}
+
+.navbar-container {
+  display: flex;
+  justify-content: space-between; /* 使菜单项均匀分布在导航栏中 */
+  align-items: center;
+  padding: 10px; /* 添加一些内边距 */
+  width: 100%; /* 让导航栏内容充满一行 */
+}
+
+.el-menu-demo {
+  background-color: transparent; /* 保持菜单背景透明，以显示导航栏的背景色 */
+  flex: 1; /* 使菜单占据可用空间 */
+}
+
+/* 可选：调整菜单项的样式 */
+.el-menu-item {
+  color: #333; /* 菜单项文字颜色 */
+  transition: all 0.3s ease; /* 添加过渡效果 */
+  letter-spacing: 5px; /* 调整字间距，可以根据需要调整具体数值 */
+  font-family: 'Arial', sans-serif; /* 更改字体为 Arial，如果没有 Arial 字体，则使用 sans-serif 作为备选字体 */
+}
+
+
+
+.el-menu-item:hover {
+  color: #00a65a; /* 鼠标悬停时的颜色，可根据你的主题颜色调整 */
 }
 </style>
--- src/pages/About.vue
+++ src/pages/About.vue
@@ -6,9 +6,10 @@
         <h2>关于我们/联系我们</h2>
       </el-header>
       <el-main>
-        <p>这里是关于我们/联系我们的内容。</p>
+
       </el-main>
     </el-container>
+
   </div>
 </template>
 
