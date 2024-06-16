f223b7b59eeaf6ec2036315ff9c49e0155c6c33d
wubo <2551921037@qq.com>
2024-06-16 20:30:35 +0800

    标志景点第一张图片

--- src/pages/Culture.vue
+++ src/pages/Culture.vue
@@ -1,24 +1,7 @@
 <template>
-  <div>
-    <Navbar />
-    <el-container>
-      <el-header>
-        <h2>历史介绍</h2>
-      </el-header>
-      <el-main>
-        <!-- 添加历史介绍的内容 -->
-        <p>这里是历史介绍的内容。</p>
-      </el-main>
-    </el-container>
-  </div>
+
 </template>
 
 <script>
-import Navbar from '@/components/AppNavbar.vue';
 
-export default {
-  components: {
-    Navbar,
-  },
-};
 </script>
--- src/pages/Landmarks.vue
+++ src/pages/Landmarks.vue
@@ -1,17 +1,42 @@
 <template>
   <div id="app" class="blue-background">
+    <div class="image-container">
+      <figure class="image-figure">
+        <img src="@/assets/标志景点/树正沟景点.png" alt="树正沟景点" class="image">
+      </figure>
+
+    </div>
+
 
   </div>
-</template>
 
+</template>
 
 <style scoped>
 /* 设置蓝色背景 */
 .blue-background {
-  background-color: blue;
-  min-height: 100vh; /* 确保背景颜色覆盖整个视口高度,没有这个就能改成蓝色 */
+  background-color: rgb(126, 202, 186);
+  min-height: 100vh; /* 确保背景颜色覆盖整个视口高度 */
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
+/* 设置图片容器的样式 */
+.image-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 
+/* 设置图片的样式 */
+.image-figure {
+  margin: 0;
+}
 
+.image {
+  max-width: 100%; /* 确保图片不会超出其容器的宽度 */
+  max-height: 80vh; /* 设置图片最大高度为视口高度的80% */
+  object-fit: contain; /* 保持图片的纵横比 */
+}
 </style>
