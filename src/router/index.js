8328270f5ee00352aa21439013183d28aeb86cfd
wubo <2551921037@qq.com>
2024-06-18 15:55:26 +0800

    自然与动物 第三张卡片

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import HomePage from '../pages/HomePage.vue';
 import About from '../pages/About.vue';
 import Landmarks from '../pages/Landmarks.vue';
 import Nature from '../pages/Nature.vue';
-import Research from '../pages/Research.vue';
+import ReligionFolklore from '../pages/ReligionFolklore.vue';
 import Culture from '../pages/Culture.vue';
 import Festivals from '../pages/Festivals.vue';
 
@@ -12,7 +12,7 @@ const routes = [
   { path: '/about', component: About },
   { path: '/landmarks', component: Landmarks },
   { path: '/nature', component: Nature },
-  { path: '/research', component: Research },
+  { path: '/religionFolklore', component: ReligionFolklore },
   { path: '/culture', component: Culture },
   { path: '/festivals', component: Festivals },
   { path: '/site/:id', component: Festivals },  // 动态路由，假设是详情页面
