e6f9fa9f7dc9f57939f53b98c6446c4f26619875
wubo <2551921037@qq.com>
2024-06-18 14:56:52 +0800

    自然与动物 解决小卡片高度不同bug

--- src/pages/Nature.vue
+++ src/pages/Nature.vue
@@ -56,10 +56,10 @@ export default {
       cards: [
         { title: "三尖杉", description: "三尖杉常绿乔木，高10～20m。树皮灰褐色至红褐色。小枝对生冬芽顶生。叶螺旋状排成2列，线状披针形，微弯下面气孔带白色。雄球花呈球形，具短柄，每个雄球花有6～16雄蕊 ，基部具1苞片；雌球花具长梗，生于枝下部叶腋，每苞有2直立胚球。", image: "src/assets/自然与动物/三尖杉.png" },
         { title: "冬虫夏草", description: "冬虫夏草又名虫草，是我国民间惯用的一种名贵滋补药材，其营养成分高于人参，可入药，也可食用，是上乘的佳肴，具有很高的营养价值。九寨沟冬虫夏草含有脂肪、蛋白质、虫草酸、对腰膝酸痛，久病体弱、贫血等有良好的补益作用。", image: "src/assets/自然与动物/冬虫夏草.png" },
-        { title: "冷杉", description: "冷杉（学名：Abies fabri （Mast.）Craib）常绿乔木，树干端直，枝条轮生。树皮灰色或深灰色，裂成不规则的薄片固着于树干上，内皮淡红色；一年生枝淡褐黄色、淡灰黄色或淡褐色。球果卵状圆柱形或短圆柱形，有短梗，熟时暗黑色或淡蓝黑色，微被白粉。", image: "src/assets/自然与动物/冷杉.png" },
+        // { title: "冷杉", description: "冷杉（学名：Abies fabri （Mast.）Craib）常绿乔木，树干端直，枝条轮生。树皮灰色或深灰色，裂成不规则的薄片固着于树干上，内皮淡红色；一年生枝淡褐黄色、淡灰黄色或淡褐色。球果卵状圆柱形或短圆柱形，有短梗，熟时暗黑色或淡蓝黑色，微被白粉。", image: "src/assets/自然与动物/冷杉.png" },
         { title: "四川红杉", description: "四川红杉（学名：Larix mastersiana）为松科落叶松属的植物，为中国的特有植物。生长于海拔2,300米至3,500米的地区，属植物中较耐阴的种类，喜温凉、湿润气候，多生于沟谷、溪边或冷杉、云杉林缘，常与其它阔叶树种组成针阔混交林。", image: "src/assets/自然与动物/四川红杉.png" },
         { title: "白皮云杉", description: "白皮云杉（学名：Picea aurantiaca）别名白皮杉（中国祼子植物志），为松科云杉属的植物，是中国的特有植物。分布在中国大陆的四川康定等地，生长于海拔2,600米至3,600米的地区。多散生，因采伐范围不断扩大，林木数量日益减少。", image: "src/assets/自然与动物/白皮云杉.png" },
-        { title: "红松", description: "红松（Pinus koraiensis Sieb. et Zucc.）裸子植物，常绿乔木，高可达40米。小枝密生褐色柔毛。叶5针一束，粗硬，树脂道3个，叶鞘早落。球果卵状圆锥形，种鳞先端钝，向外反曲，成熟时种子不脱落。常同鱼鳞松、红皮云杉组成混交林。耐寒性强，喜微酸性土或中性土。", image: "src/assets/自然与动物/红松.png" },
+        // { title: "红松", description: "红松（Pinus koraiensis Sieb. et Zucc.）裸子植物，常绿乔木，高可达40米。小枝密生褐色柔毛。叶5针一束，粗硬，树脂道3个，叶鞘早落。球果卵状圆锥形，种鳞先端钝，向外反曲，成熟时种子不脱落。常同鱼鳞松、红皮云杉组成混交林。耐寒性强，喜微酸性土或中性土。", image: "src/assets/自然与动物/红松.png" },
         { title: "连香树", description: "连香树为连香树科连香树属。落叶乔木，高10—20（—40）米，胸径达1米；有长枝和矩状短枝，该种为第三纪古热带植物的孑遗种单科植物，是较古老原始的木本植物，雌雄异株，结实较少，天然更新困难，资源稀少，已濒临灭绝状态。", image: "src/assets/自然与动物/连香树.png" },
         { title: "领春木", description: "领春木(Euptelea pleiosperma)，别名：云叶树、正心木和木桃，隶属领春木科领春木属植物，落叶小乔木，对于研究古植物区系和古代地理气候有重要的学术价值。属于国家三级保护植物。多分布于海拔720—3600m的湿润沟谷地两侧。", image: "src/assets/自然与动物/领春木.png" },
         { title: "黄栌", description: "黄栌（Cotinus coggygria Scop.）别名红叶、红叶黄栌、黄道栌、黄溜子、黄龙头、黄栌材、黄栌柴、黄栌会等，是中国重要的观赏红叶树种，叶片秋季变红，鲜艳夺目。黄栌花后久留不落的不孕花的花梗呈粉红色羽毛状在枝头形成似云似雾的景观。", image: "src/assets/自然与动物/黄栌.png" },
@@ -149,7 +149,7 @@ export default {
 
 .large-card {
   width: 1000px;
-  height: 600px;
+  height: 650px;
   position: absolute; /* 绝对定位 */
   top: 600px; /* 调整大卡片的顶部位置 */
   left: 250px; /* 调整大卡片的左侧位置 */
@@ -160,6 +160,21 @@ export default {
   background-color: white; /* 修改背景颜色为白色 */
   border-radius: 15px;
   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+}
+
+.small-card {
+  flex: 0 0 calc(33.33% - 10px); /* 调整小卡片的宽度，并减去间隔 */
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+//align-items: stretch; /* 使内容占满整个卡片 */
+  box-sizing: border-box;
+  padding: 20px; /*不加这个卡片高度不一样*/
+  background: white;
+  border-radius: 10px;
+  margin: 0 5px;
+  backdrop-filter: blur(10px);
 }
 
 .header {
@@ -185,20 +200,6 @@ export default {
   overflow-x: auto; /* 如果需要水平滚动，可以添加这个 */
   width: 100%;
   justify-content: center;
-}
-.small-card {
-  flex: 0 0 calc(33.33% - 10px); /* 调整小卡片的宽度，并减去间隔 */
-  height: 100%; /* 填满容器高度 */
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  //align-items: stretch; /* 使内容占满整个卡片 */
-  box-sizing: border-box;
-  padding: 10px; /*不加这个卡片高度不一样*/
-  background: white;
-  border-radius: 10px;
-  margin: 0 5px;
-  backdrop-filter: blur(10px);
 }
 
 .image {
@@ -240,11 +241,11 @@ export default {
 }
 
 .left-arrow {
-  left: 10px;
+  left: -50px;
 }
 
 .right-arrow {
-  right: 10px;
+  right: -50px;
 }
 
 
